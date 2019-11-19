7acb6842f3d21cd6189a1d4ebb009766898f7eb0
Manuel Guerrini <man.guerrini@gmail.com>
2019-11-19 17:25:37 +0100

    Stampo in pagina con handlebars (no css)

--- myscript.js
+++ myscript.js
@@ -7,10 +7,18 @@ $.ajax ({
     console.log("artista " + artista); // stampo l'artista che esce
 
     // serve un ciclo per stampare gli artisti
-    var source = $("#entry-template").html();
-    console.log("source " + source);
-    var template = Handlebars.compile(source);
-    console.log("template " + template);
+    for (var i = 0; i < artista.length; i++) { // ciclo gli artisti fino a quando ci sono artisti nell'api, per stampare tutto
+
+      var source = $("#entry-template").html(); //#entry-template è il contenitore handlebars
+      console.log("source " + source);
+      var template = Handlebars.compile(source); //compilo il template
+      console.log("template " + template);
+
+      var html = template(artista[i]); //"creo" l'html dell'iesimo artista
+      console.log('html ' + html);
+      $('.container').append(html) //appendo html all'interno del container di pagina, così ogni artista è uno dopo l'altro
+    }
+
 
   }
 
